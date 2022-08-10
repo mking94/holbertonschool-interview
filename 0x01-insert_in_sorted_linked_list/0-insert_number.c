5c95c13e658ded34277ed5d37c98021d344a760f
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-08-10 12:17:24 +0200

    C function that inserts a number into a sorted singly linked list.

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -9,28 +9,26 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new, *node;
-	node = *head;
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-	if (*head == NULL || (*head)->n >= new->n)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
-
-	for (node = *head; node != NULL; node = node->next)
-	{
-		if (node->next == NULL || node->next->n > new->n)
-		{
-
-			new->next = node->next;
-			node->next = new;
-			break;
-		}
-	}
-	return (new);
+listint_t *new, *node;
+node = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+return (NULL);
+new->n = number;
+if (*head == NULL || (*head)->n >= new->n)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
+for (node = *head; node != NULL; node = node->next)
+{
+if (node->next == NULL || node->next->n > new->n)
+{
+new->next = node->next;
+node->next = new;
+break;
+}
+}
+return (new);
 }
