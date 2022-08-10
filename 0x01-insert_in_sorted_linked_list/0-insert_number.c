952cc834071495548db7541a512c3540ee40622c
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-08-10 11:59:08 +0200

    C function that inserts a number into a sorted singly linked list.

--- 0x01-insert_in_sorted_linked_list/0-insert_number.c
+++ 0x01-insert_in_sorted_linked_list/0-insert_number.c
@@ -11,14 +11,14 @@ listint_t *insert_node(listint_t **head, int number)
 {
 if (head == null)
 {
-return null;
+return (null);
 }
 while (head->next != NULL)
 {
 listint_t *next = head->next;
 if (head->n <= number && next->n > number)
 {
-listint_t *new = malloc(sizeof(listint_t)); 
+listint_t *new = malloc(sizeof(listint_t));
 new->n = number;
 new->next = next->next;
 head->next = new;
