887d7c114b0166f660ac643cf46535316ebc0532
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-08-18 10:42:48 +0200

    Function that creates a binary tree node

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -10,12 +10,14 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *nodeNew;
-	nodeNew = malloc(sizeof(binary_tree_t));
-	if (nodeNew == NULL)
-		return (NULL);
-	nodeNew->left = nodeNew->right = NULL;
-	nodeNew->n = value;
-	nodeNew->parent = parent;
-	return (nodeNew);
+binary_tree_t *nodeNew;
+nodeNew = malloc(sizeof(binary_tree_t));
+if (nodeNew == NULL)
+{
+return (NULL);
+}
+nodeNew->left = nodeNew->right = NULL;
+nodeNew->n = value;
+nodeNew->parent = parent;
+return (nodeNew);
 }
