d08829cd1ff674affe7b1ef278c85c9b762c628c
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-08-25 20:17:15 +0200

    Minimum Operations

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Minimum operations  function
+"""
+def countProcess(num):
+    """ Return list of process until n H """
+    con = 1
+    p_list = []
+    val = num
+    while val != 1:
+        con += 1
+        if val % con == 0:
+            while (val % con == 0 and val != 1):
+                val /= con
+                p_list.append(con)
+
+    return p_list
+
+
+def minOperations(n):
+    """ Return sum of process until n H """
+    if n < 2 or type(n) is not int:
+        return 0
+    values = countProcess(n)
+    return sum(values)
