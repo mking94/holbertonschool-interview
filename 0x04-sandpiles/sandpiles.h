d112c0427c37f25dc3593820815e0480d1e2a2ac
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-01 11:08:14 +0200

    Header file

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef SANDPILES_H
+#define SANDPILES_H
 #include <stdlib.h>
 #include <stdio.h>
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
