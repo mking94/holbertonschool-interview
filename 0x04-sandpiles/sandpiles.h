86eedff54f041c6daf303843a1a6f39035d1ace6
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-01 11:52:16 +0200

    Header file

--- 0x04-sandpiles/sandpiles.h
+++ 0x04-sandpiles/sandpiles.h
@@ -4,10 +4,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void grid_addition(int grid1[3][3], int grid2[3][3]);
+void grid_print(int grid[3][3]);
+int grid_is_ok(int grid[3][3]);
+void grid_change(int grid1[3][3]);
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void sum_grids(int grid1[3][3], int grid2[3][3]);
-void change_grids(int grid1[3][3]);
-int check_grids(int grid1[3][3]);
 
 #endif
 
