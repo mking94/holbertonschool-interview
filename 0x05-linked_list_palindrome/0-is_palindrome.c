36827bb75ca4111645da731f7e23633729c1172b
mking94 <dadimaher94@gmail.com>
2022-09-08 03:33:49 -0700

    C Function checks if a singly linked list is a palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -7,20 +7,20 @@
  * @head: pointer to head of list
  * Return: True if linked list is a palindrome
  */
- int is_palindrome(listint_t **head)
- {
- int i = 0;
- int tab[1024];
- while (head->next != null)
- {
- tab[i] = head->n;
- i++;
- head = head->next;
- }
- for (int j = 0; j < (i / 2); j++)
- {
- if (tab[j] != tab[i - j])
- return 0;
- }
- return 1;
- }
+int is_palindrome(listint_t **head)
+{
+int i = 0;
+int tab[1024];
+while (head->next != null)
+{
+tab[i] = head->n;
+i++;
+head = head->next;
+}
+for (int j = 0; j < (i / 2); j++)
+{
+if (tab[j] != tab[i - j])
+return 0;
+}
+return 1;
+}
