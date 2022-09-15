cefa24cfc4aa97cb78c70d76e315ca9161823691
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-15 12:05:01 +0200

    Script that reads stdin line by line and computes metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-""" Log parsing """
+"""
+read stdin line by line and computes metrics
+"""
 import sys
-
-
-i = 0
-FileSize = 0
 STATUS = {'200': 0, '301': 0,
           '400': 0, '401': 0,
           '403': 0, '404': 0,
           '405': 0, '500': 0}
+size = 0
+lines = 0
 try:
-    for line in sys.stdin:
-        i += 1
-        sp = line.split(' ')
-        if len(sp) > 2:
-            FileSize += int(sp[-1])
-            if sp[-2] in STATUS:
-                STATUS[sp[-2]] += 1
-        if i % 10 == 0:
-            print("File size: {}".format(FileSize))
-            for key, value in sorted(STATUS.items()):
-                if value != 0:
-                    print("{}: {}".format(key, value))
+    for argument in sys.stdin:
+        argumentList = argument.split(" ")
+        if len(argumentList) > 2:
+            status = argumentList[-2]
+            fileSize = int(argumentList[-1])
+            if status in STATUS:
+                STATUS[status] += 1
+            size += fileSize
+            lines += 1
+            if lines == 10:
+                print("File size: {:d}".format(size))
+                for key, value in sorted(STATUS.items()):
+                    if value != 0:
+                        print("{}: {:d}".format(key, value))
+                lines = 0
 finally:
-    print("File size: {}".format(FileSize))
+    print("File size: {:d}".format(size))
     for key, value in sorted(STATUS.items()):
-            if value != 0:
-                print("{}: {:d}".format(key, value))
+        if value != 0:
+            print("{}: {:d}".format(key, value))
