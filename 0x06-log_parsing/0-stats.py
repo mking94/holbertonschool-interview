b2ab4a2b7154252ff5874a3b298d9c968f67b83a
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-15 12:02:13 +0200

    Script that reads stdin line by line and computes metrics:

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,48 +1,29 @@
 #!/usr/bin/python3
-"""
-Task - Script that reads stdin line by line and computes metrics
-"""
-
+""" Log parsing """
 import sys
 
 
-if __name__ == "__main__":
-    st_code = {"200": 0,
-               "301": 0,
-               "400": 0,
-               "401": 0,
-               "403": 0,
-               "404": 0,
-               "405": 0,
-               "500": 0}
-    count = 1
-    file_size = 0
-
-    def parse_line(line):
-        """ Read, parse and grab data"""
-        try:
-            parsed_line = line.split()
-            status_code = parsed_line[-2]
-            if status_code in st_code.keys():
-                st_code[status_code] += 1
-            return int(parsed_line[-1])
-        except Exception:
-            return 0
-
-    def print_stats():
-        """print stats in ascending order"""
-        print("File size: {}".format(file_size))
-        for key in sorted(st_code.keys()):
-            if st_code[key]:
-                print("{}: {}".format(key, st_code[key]))
-
-    try:
-        for line in sys.stdin:
-            file_size += parse_line(line)
-            if count % 10 == 0:
-                print_stats()
-            count += 1
-    except KeyboardInterrupt:
-        print_stats()
-        raise
-    print_stats()
+i = 0
+FileSize = 0
+STATUS = {'200': 0, '301': 0,
+          '400': 0, '401': 0,
+          '403': 0, '404': 0,
+          '405': 0, '500': 0}
+try:
+    for line in sys.stdin:
+        i += 1
+        sp = line.split(' ')
+        if len(sp) > 2:
+            FileSize += int(sp[-1])
+            if sp[-2] in STATUS:
+                STATUS[sp[-2]] += 1
+        if i % 10 == 0:
+            print("File size: {}".format(FileSize))
+            for key, value in sorted(STATUS.items()):
+                if value != 0:
+                    print("{}: {}".format(key, value))
+finally:
+    print("File size: {}".format(FileSize))
+    for key, value in sorted(STATUS.items()):
+            if value != 0:
+                print("{}: {:d}".format(key, value))
