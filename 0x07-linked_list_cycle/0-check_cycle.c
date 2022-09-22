2177f05db156a2584528fccd096fbc49fab55cb5
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-22 09:40:36 +0200

    Function that check linked list cycle

--- 0x07-linked_list_cycle/0-check_cycle.c
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -9,11 +9,12 @@
 
 int is_exist(int arg0, int[] arg1)
 {
-    size_t arraylength = sizeof(arg1)/sizeof(arg1[0]);
-    for(size_t i = 0; i < arraylength; i++) {
-        if(arg0 == arg1[i]) return 1;
-    }
-    return 0;
+size_t arraylength = sizeof(arg1) / sizeof(arg1[0]);
+for(size_t i = 0; i < arraylength; i++)
+{
+if(arg0 == arg1[i]) return 1;
+}
+return 0;
 }
 
 /**
@@ -21,19 +22,21 @@ int is_exist(int arg0, int[] arg1)
  * @list: The element to be checked.
  * Retuen: 1 if cycle or 0 if not.
  */
- 
-int check_cycle(listint_t *list) {
-    struct listint_t* current = list;
-    int tab[1024];
-    int i = 0;
-    while (current != NULL)
-    {
-        tab[i] = (int) &current;
-        current = current->next;
-        if(is_exist((int) &current,tab)){
-            return 1;
-        }
-		i++;
-    }
-    return 0;
+
+int check_cycle(listint_t *list)
+{
+struct listint_t* current = list;
+int tab[1024];
+int i = 0;
+while (current != NULL)
+{
+tab[i] = (int) &current;
+current = current->next;
+if(is_exist((int) &current,tab))
+{
+return 1;
+}
+i++;
+}
+return 0;
 }
