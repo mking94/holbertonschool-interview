6629d280d91ad3ee4084325b4a445f0628b35481
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-09-28 10:24:52 +0200

    Function that checks whether or not a given unsigned integer is a palindrome

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -1,21 +1,5 @@
 #include "palindrome.h"
 
-/**
- * strrev - Revers a string
- * @str: The input.
- * Return: The revers input as a string.
- */
-
-char *strrev(char *str)
-{
-char ch[strlen(str)];
-for (int i = 0; i < (int)strlen(str); i++)
-{
-ch[i] = str[strlen(str) - i - 1];
-}
-str = ch;
-return (str);
-}
 
 /**
  * is_palindrome - Check given unsigned integer is a palindrome.
@@ -25,9 +9,14 @@ return (str);
 
 int is_palindrome(unsigned long n)
 {
-char str[80];
-sprintf(str, "%lu", n);
-if (strcmp(str, strrev(str)) == 0)
+unsigned long x = 0, aux = n;
+while(aux != 0)
+{
+x = x + (aux % 10);
+aux = aux / 10;
+x = x * 10;
+}
+if ((x / 10) == n)
 {
 return (1);
 }
