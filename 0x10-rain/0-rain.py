c2f9dfbe236740f197f51624c16d916e1be76887
mking94 <dadimaher94@gmail.com>
2022-11-30 00:30:17 -0800

    Rain Algorithem

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Rain Algorithm """
+
+
+def rain(walls):
+    """ Function that calculates water retained """
+    if not isinstance(walls, list) and len(walls) < 2:
+        return 0
+
+    if not all(isinstance(n, int) for n in walls):  # any no integer
+        return 0
+
+    rain = 0
+    for i in range(1, len(walls) - 1):
+
+        # Find the maximum element on its left
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        # Find the maximum element on its right
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+
+        # Update the maximum of rain collected
+        rain += (min(left, right) - walls[i])
+
+    return rain
