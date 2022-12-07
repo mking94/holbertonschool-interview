6021082513f84e250b0c41e2478e3076f421bc41
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2022-12-07 11:39:30 +0100

    Header file

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-heapify(int arr[], size_t size, int i);
+void heapify(int arr[], size_t size, int i);
 void heap_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 #endif
