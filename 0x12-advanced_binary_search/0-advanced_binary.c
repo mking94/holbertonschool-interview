47f77e376dce6a4c9bfd3714dc9fda459e83b8bf
mking94 <dadimaher94@gmail.com>
2022-12-14 07:40:44 -0800

    Update

--- 0x12-advanced_binary_search/0-advanced_binary.c
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -1,44 +1,71 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * advanced_binary - Function that the index where value is located
- * @array: The array input
- * @size: Array size
- * @value: value that want to find index
+ * print_array - Entry point
  *
- * Return: The index where value is located
+ * @array: Array
+ * @first: Size of array
+ * @last: Value to find
+ * Return: Always EXIT_SUCCESS
+ */
+void print_array(int *array, int first, int last)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = first; i < last; i++)
+		printf("%d, ", array[i]);
+	printf("%d", array[i]);
+	printf("\n");
+}
+
+/**
+ * advanced_binary - Entry point
+ *
+ * @array: Array
+ * @size: Size of array
+ * @value: Value to find
+ * Return: Always EXIT_SUCCESS
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-if (value == NULL)
-{
-return (-1);
+	size_t first;
+	size_t last;
+
+	if (!array)
+		return (-1);
+
+	first = 0;
+	last = size - 1;
+	return (recursive_search(array, first, last, value));
+
 }
-int aux = (int)size / 2;
-int compteur = 0;
-do {
-if (array[aux] > value)
+
+
+/**
+ * recursive_search - Recursive advanced binary search
+ * @array: Array
+ * @first: First element of array
+ * @last: Last element of array
+ * @value: Value to find
+ * Return: Always EXIT_SUCCESS
+ */
+int recursive_search(int *array, size_t first, size_t last, int value)
 {
-for (int i = compteur; i < aux; i++)
-{
-if (array[i] == value)
-{
-return (i);
-}
-}
-compteur = aux;
-aux = compteur + ((int)((int)size - aux) / 2);
-}
-else
-{
-compteur = aux;
-aux = compteur + ((int)((int)size - aux) / 2) + 1;
-}
-if (compteur >= size)
-{
-return (-1);
-}
-} while (compteur < size);
+	size_t half;
+
+	if (first < last)
+	{
+		half = first + (last - first) / 2;
+		print_array(array, (int)first, (int)last);
+		if (array[half] >= value)
+			return (recursive_search(array, first, half, value));
+		else
+			return (recursive_search(array, half + 1, last, value));
+		return ((int)(half));
+	}
+	if (array[first] == value)
+		return (first);
+	print_array(array, (int)first, (int)last);
+	return (-1);
 }
