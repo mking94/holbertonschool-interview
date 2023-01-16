f35b746028233acc9cf0f6c19273d351cc663363
mking94 <dadimaher94@gmail.com>
2023-01-16 16:13:43 +0100

    Rotate 2D Matrix

---
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,12 @@
+def rotate_2d_matrix(matrix):
+    i = len(matrix) - 1
+    aux = {}
+    j = 0
+    while i >= 0:
+        for x in range(0, len(matrix[i])):
+            aux["[{}][{}]".format(x, j)] = matrix[i][x]
+        j += 1
+        i -= 1
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[i])):
+            matrix[i][j] = aux["[{}][{}]".format(i, j)]
