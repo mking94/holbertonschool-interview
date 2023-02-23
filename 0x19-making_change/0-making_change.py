4618c640f4643c3e36a9e4b2180c0ab5cc123486
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2023-02-23 15:19:27 +0100

    Update 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
 """
-Change comes from within
+Task: Change comes from within
+Given a pile of coins of different values,
+determine the fewest number of coins needed to
+meet a given amount total
 """
 
 
 def makeChange(coins, total):
     """
-    ********************************************
-    ****determine the fewest number of coins****
-    *******needed to meet a given amount********
-    ********************************************
-    @coins: is a list of the values of the coins
-            in the possession
-    @total: given amount
-    Return:
-            fewest number of coins needed to meet total
-            *** If total is 0 or less, return 0
-            *** If total cannot be met by any number
-                of coins you have, return -1
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Return: fewest number of coins needed to meet total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
     """
-    if total < 0:
+    if total <= 0:
         return 0
-    coins.sort()
-    coins.reverse()
-    fewest = 0
-    for coin in coins:
-        """ For loop """
-        if total <= 0:
-            break
-        buff = total // coin
-        fewest += buff
-        total -= (buff * coin)
-    if total != 0:
+
+    newVal = total + 1
+    store = {0: 0}
+
+    for i in range(1, total + 1):
+        store[i] = newVal
+
+        for coin in coins:
+            current = i - coin
+            if current < 0:
+                continue
+
+            store[i] = min(store[current] + 1, store[i])
+
+    if store[total] == total + 1:
         return -1
-    return fewset
+
+    return store[total]
