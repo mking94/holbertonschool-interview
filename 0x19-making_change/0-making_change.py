ecc0109496d0c51db00d023a993221dc053fec82
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2023-02-23 14:14:17 +0100

    Update 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,41 +1,18 @@
 #!/usr/bin/python3
-"""
-0x19. Making Change, task 0. Change comes from within
-"""
+'''Making Change module'''
 
 
 def makeChange(coins, total):
-    """
-    Given a set of coin denominations, determines the fewest coins needed to
-    meet a given total value.
-    Args:
-        coins (list of (int)s): list of coin denominations available
-        total (int): target total value
-    Return:
-        total amount of coins needed to meet `total` value
-    """
-    if type(total) is not int or type(coins) is not list or not all(
-            [type(coin) is int for coin in coins]):
-        print("Invalid args")
+    '''Function that makes change for a given total'''
+    if total < 1:
         return 0
-
-    if total <= 0:
-        return 0
-
+    change = 0
     coins.sort(reverse=True)
-
-    total_coins = 0
-    remainder = total
-    for denom in coins:
-        if denom <= remainder:
-            curr = remainder // denom
-            total_coins += curr
-            remainder -= curr * denom
-        if remainder == 0:
-            break
-
-    if total_coins == 0 or remainder != 0:
+    for coin in coins:
+        temp_change = int(total / coin)
+        total -= (temp_change * coin)
+        change += temp_change
+        if total == 0:
+            return change
+    if total != 0:
         return -1
-
-    return total_coins
-
