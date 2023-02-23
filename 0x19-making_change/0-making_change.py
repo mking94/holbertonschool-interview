ee98bee3e59a1ba75719ae514df0c638b757ff52
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2023-02-23 15:24:57 +0100

    Update 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -5,6 +5,7 @@ Given a pile of coins of different values,
 determine the fewest number of coins needed to
 meet a given amount total
 """
+import sys
 
 
 def makeChange(coins, total):
@@ -22,20 +23,18 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    newVal = total + 1
-    store = {0: 0}
+    max_n = sys.maxsize
+    hash_t = [max_n for i in range(total + 1)]
+    hash_t[0] = 0
+    l = len(coins)
+    for d in range(1, total + 1):
+        for i in range(l):
+            if coins[i] <= d:
+                res = hash_t[d - coins[i]]
+                if res != max_n and res + 1 < hash_t[d]:
+                    hash_t[d] = res + 1
 
-    for i in range(1, total + 1):
-        store[i] = newVal
-
-        for coin in coins:
-            current = i - coin
-            if current < 0:
-                continue
-
-            store[i] = min(store[current] + 1, store[i])
-
-    if store[total] == total + 1:
+    if hash_t[total] == max_n:
         return -1
 
-    return store[total]
+    return hash_t[total]
