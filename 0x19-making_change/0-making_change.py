6618eba26b4807b8476e317c82b06fea0f3ba250
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2023-02-23 16:01:10 +0100

    Update 0-making_change.py

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -5,17 +5,21 @@ Return the least number of coins need to return the total
 
 
 def makeChange(coins, total):
-    """Return the least number of coins need to return the total
+    """
+    Return the least number of coins need to return the total
     @coins: list of the coins available
     @total: total to return
     Return: least number of coins needed to get to total
     """
     num_of_coins = 0
+
     if total <= 0:
         return 0
+
     for coin in sorted(coins)[::-1]:
         num_of_coins += total // coin
         total = total % coin
     if total != 0:
         return -1
+
     return num_of_coins
