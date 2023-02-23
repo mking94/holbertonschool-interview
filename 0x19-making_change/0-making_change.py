d976f1a7d49bbe83546b86550644d6574e67a255
Maher Ben dada <47383456+mking94@users.noreply.github.com>
2023-02-23 16:05:06 +0100

    Change comes from within

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """
 **********~Making change~**********
 Return the least number of coins need to return the total
@@ -7,10 +8,16 @@ Return the least number of coins need to return the total
 
 def makeChange(coins, total):
     """
-    Return the least number of coins need to return the total
+    Change comes from within
     @coins: list of the coins available
     @total: total to return
     Return: least number of coins needed to get to total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
     """
     num_of_coins = 0
 
