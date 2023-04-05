a7a1c55b6552391f89dfffcaf6a649de0e9b2cb1
mking94 <dadimaher94@gmail.com>
2023-04-05 13:30:49 +0200

    Function that return pascal triangle

--- 0x1F-pascal_triangle/0-pascal_triangle.py
+++ 0x1F-pascal_triangle/0-pascal_triangle.py
@@ -6,10 +6,10 @@ def pascal_triangle(n):
     if n <= 0:
         return []
     pas_tri = [[1]]
-    for i in range(0,n):
+    for i in range(0, n):
         aux = [1]
-        for j in range(1,i):
-            aux.append(pas_tri[i][j] + pas_tri[i][j-1])
+        for j in range(1, i):
+            aux.append(pas_tri[i][j] + pas_tri[i][j - 1])
         aux.append(1)
         pas_tri.append(aux)
     del pas_tri[1]
